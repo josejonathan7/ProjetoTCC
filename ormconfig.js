031d5259549e9a45d45ff301bb158468bc18b957
josé jonathan <josejonathan689@gmail.com>
2021-08-01 22:12:14 -0300

    configurando...

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,9 @@
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
+    "ssl": {
+        rejectUnauthorized: false
+    },
     "entities": [ "dist/entities/*.js" ],
     "migrations": [ "dist/database/migrations/*.js" ],
     "cli":{
